afee2b97b725a06ea21afd46c63322ae3a40e3fe
Dilki Welagedara <dilkipw1218@gmail.com>
2023-05-03 12:05:21 +0530

    Education Basic pages added

---
+++ Frontend/src/pages/health/healthHome.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const healthHome = () => {
+  return (
+    <div>healthHome</div>
+  )
+}
+
+export default healthHome
--- Frontend/src/pages/index.jsx
+++ Frontend/src/pages/index.jsx
@@ -1,7 +1,19 @@
 import HealthHome from "./health/healthHome";
 import DonateAdvertisement from "./health/donateAdvertisement";
 
+// Education Pages
+import EducationList from "./education-list";
+import EducationAdd from "./education-add";
+import EducationOne from "./education-one";
+import EducationUpdate from "./education-update";
+
 export {
-    HealthHome,
-    DonateAdvertisement
+  HealthHome,
+  DonateAdvertisement,
+
+  // Education Pages
+  EducationList,
+  EducationAdd,
+  EducationOne,
+  EducationUpdate,
 };
